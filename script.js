d346b2717c64ef784656729a442d9308595eee89
oley-projects <oley.for.git@gmail.com>
2021-06-09 17:39:49 +0300

    difficulty levels, local storage

--- script.js
+++ script.js
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in local storage or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
 // Focus on text on start
 text.focus();
 
@@ -69,7 +75,7 @@ function updateTime() {
     time--;
     timeEl.innerHTML = time + 's';
 
-    if(time === 0) {
+    if (time === 0) {
         clearInterval(timeInterval);
         // End game
         gameOver();
@@ -91,18 +97,34 @@ addWordToDOM();
 
 // Event listeners
 
+// Typer
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
 
-    if(insertedText === randomWord) {
+    if (insertedText === randomWord) {
         addWordToDOM();
         updateScore();
 
         // Clear input
         e.target.value = '';
 
-        time += 5;
+        if (difficulty === 'hard') {
+            time += 2;
+        } else if (difficulty === 'medium') {
+            time += 3;
+        } else {
+            time += 5;
+        }
 
         updateTime();
     }
+});
+
+// Settings button click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
 });
